86beed364443e2a32daccefc33f96cef5366d3e4
VihoSerge <komi.vihoserge@gmail.com>
2018-12-29 09:05:59 +0000

    HW1

---
+++ HW1/song.js
@@ -0,0 +1,35 @@
+/*
+* I declare the different attributes to represent my favorite song
+*/
+
+//The song's title
+var title = "Kuliko Jana";
+
+//The name of the artist
+var artist = "Sauti Sol";
+
+//The album of the song
+var album = "Live And Die In Afrika";
+
+//The year of release
+var year = 2016;
+
+// Genre
+var genre = "Afro";
+
+// Duration of the song
+var durationInSeconds = 332;
+
+// Those who collaborate on the song
+var featuring = ["RedFourth Chorus"];
+
+
+// Display of attributes
+
+console.log("Title : " + title);
+console.log("Artist : " + artist);
+console.log("Album : " + album);
+console.log("Year : " + year);
+console.log("Genre : " + genre);
+console.log("Duration : " + durationInSeconds);
+console.log("Featuring : " + featuring);
